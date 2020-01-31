eb612317d805868ba59fc9f95cefeea025da2bd5
Eun Jung Choi <im.your.energy@gmail.com>
2020-01-31 11:37:58 +0900

    Add header and main component

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="header d-flex w-100" style="height: 4.5rem; border-bottom: 1px solid lightgrey">
+        <h1>
+            <img alt="Vue logo" src="../assets/logo-text.png" style="width: 120px; height: auto; margin-left: 1rem">
+        </h1>
+        <div style="position: relative">
+            <span class="account"></span>
+        </div>
+    </header>
+</template>
---
+++ src/components/Main.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="main_content h-100" style="flex: auto; overflow-x: hidden">
+        <div :class="$route.name" class="h-100">
+            <b-container v-if="$store.state.token" class="h-100">
+                <router-view />
+            </b-container>
+        </div>
+    </div>
+</template>
