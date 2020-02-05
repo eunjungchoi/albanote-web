231eeff160f8370a0c400b766169178a56568e9e
Eun Jung Choi <im.your.energy@gmail.com>
2020-02-05 21:46:49 +0900

    Add save start date and insurance info when register member

--- src/views/AddMember.vue
+++ src/views/AddMember.vue
@@ -3,13 +3,32 @@
     <h6 class="my-4" v-if="member">{{ member.business.license_name }}</h6>
     <hr>
     <div v-if="!added">
-      <h5 class="py-4"><strong>직원을 등록하세요</strong></h5>
+      <h5 class="py-3"><strong>직원을 등록하세요</strong></h5>
       <b-form @submit.prevent="add">
       <b-form-group label="직원 아이디" label-for="user-id">
         <b-form-input id="user-id" v-model="form.user_id" type="text"></b-form-input>
       </b-form-group>
       <b-form-group label="시급 (단위: 원)" label-for="hourly-wage">
         <b-form-input id="hourly-wage" v-model="form.hourly_wage" type="text"></b-form-input>
+      </b-form-group>
+      <b-form-group label="주휴일" label-for="weekly_holiday">
+            <b-form-radio-group
+              id="weekly_holiday"
+              v-model="form.weekly_holiday"
+              :options="dayOptions"
+              name="weekly_holiday">
+            </b-form-radio-group>
+        </b-form-group>
+      <b-form-group label="입사일" label-for="start_date">
+        <b-form-input type="date" v-model="form.start_date"></b-form-input>
+      </b-form-group>
+      <b-form-group label="4대 보험 여부" label-for="insurance">
+        <b-form-checkbox-group
+        id="insurance"
+        v-model="form.insurance"
+        :options="options"
+        name="insurance"
+      ></b-form-checkbox-group>
       </b-form-group>
       <b-button class="btn-block btn-lg cashnote-green" type="submit">추가</b-button>
     </b-form>
@@ -21,6 +40,8 @@
   </div>
 </template>
 <script>
+import moment from 'moment'
+
 export default {
   name: 'addMember',
   data () {
@@ -28,8 +49,26 @@ export default {
       member: null,
       form: {
         user_id: null,
-        hourly_wage: 0
+        hourly_wage: 0,
+        weekly_holiday: 6,
+        insurance: [],
+        start_date: moment().format('YYYY-MM-DD').toString()
       },
+      options: [
+        { text: '국민연금', value: '0' },
+        { text: '건강보험', value: '1' },
+        { text: '고용보험', value: '2' },
+        { text: '산재보험', value: '3' }
+      ],
+      dayOptions: [
+        { text: '월', value: '0' },
+        { text: '화', value: '1' },
+        { text: '수', value: '2' },
+        { text: '목', value: '3' },
+        { text: '금', value: '4' },
+        { text: '토', value: '5' },
+        { text: '일', value: '6' }
+      ],
       added: false,
       successMessage: '직원이 추가됐습니다.',
       newlyAddedMember: null
